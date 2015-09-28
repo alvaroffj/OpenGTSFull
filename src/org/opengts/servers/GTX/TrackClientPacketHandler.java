c3d464c943995f9aecd3dd02f4a877462dfeeb15
Alvaro Flores <super.neeph@gmail.com>
2015-09-27 22:58:09 -0300

    ...

--- src/org/opengts/servers/GTX/TrackClientPacketHandler.java
+++ src/org/opengts/servers/GTX/TrackClientPacketHandler.java
@@ -495,7 +495,7 @@ public class TrackClientPacketHandler
         // byte la[] = StringTools.parseHex(l, null);
         // String las = new String(la);
         // Print.logInfo("l: " + las);
-        String id = fld[0].substring(6, 13);
+        String id = fld[0].substring(7, 13);
         Print.logInfo("id: " + id);
         id = this.hexToString(id);
         Print.logInfo("id: " + id);
